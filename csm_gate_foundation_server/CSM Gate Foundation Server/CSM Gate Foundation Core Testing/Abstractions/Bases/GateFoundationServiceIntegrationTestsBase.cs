4db2a913d492a5aa428224d3427a07a9d158e1b7
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2026-03-05 08:28:07 -0800

    adding integration tests

---
+++ csm_gate_foundation_server/CSM Gate Foundation Server/CSM Gate Foundation Core Testing/Abstractions/Bases/GateFoundationServiceIntegrationTestsBase.cs
@@ -0,0 +1,33 @@
+﻿using CSM_Database_Core.Entities.Abstractions.Interfaces;
+
+using CSM_Security_Database_Core;
+
+using CSM_Server_Core.Abstractions.Interfaces;
+
+using CSM_Server_Core_Testing.Abstractions.Bases;
+
+namespace CSM_Gate_Foundation_Core_Testing.Abstractions.Bases;
+
+/// <summary>
+///     Represents an integration tests class for {Gate Foundation} service.
+/// </summary>
+/// <typeparam name="TService">
+///     Type of the <see cref="IService"/> tested.
+/// </typeparam>
+/// <typeparam name="TEntity">
+///     Type of the <see cref="IEntity"/>
+/// </typeparam>
+public abstract class GateFoundationServiceIntegrationTestsBase<TService, TEntity>
+    : ServiceIntegrationTestsBase<TService, TEntity>
+    where TService : IService<TEntity>
+    where TEntity : class, IEntity, new() {
+
+
+    public GateFoundationServiceIntegrationTestsBase()
+        : base(
+                [
+                    () => new SecurityDatabase()
+                ]
+            ) {
+    }
+}
---
+++ csm_gate_foundation_server/CSM Gate Foundation Server/CSM Gate Foundation Core Testing/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace CSM_Gate_Foundation_Core_Testing;
+
+public class Class1 {
+
+}
