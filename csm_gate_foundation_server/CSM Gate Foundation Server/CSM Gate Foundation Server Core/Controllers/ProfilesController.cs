f729bbb5a4201739909fc95592e9e81f3b63c2cc
jr110 <c.d@hotmail.es>
2026-06-24 20:06:36 -0700

    {server} package updates fixes and adding missing files

---
+++ csm_gate_foundation_server/CSM Gate Foundation Server/CSM Gate Foundation Server Core/Controllers/ProfilesController.cs
@@ -0,0 +1,24 @@
+﻿using CSM_Gate_Foundation_Core.Services.Abstractions.Interfaces;
+
+using CSM_Security_Database_Core.Entities;
+
+using CSM_Server_Core.Abstractions.Bases;
+using CSM_Server_Core.Core.Attributes;
+
+namespace CSM_Gate_Foundation_Server_Core.Controllers;
+
+/// <summary>
+/// 
+/// </summary>
+[Feature("Profiles")]
+public class ProfilesController
+    : EntityControllerBase<IProfilesService, Profile> {
+
+    /// <summary>
+    ///     
+    /// </summary>
+    /// <param name="service"></param>
+    public ProfilesController(IProfilesService service)
+        : base(service) {
+    }
+}
